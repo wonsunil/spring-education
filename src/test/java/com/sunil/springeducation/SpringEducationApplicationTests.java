eaa4e06ff1974352f1c5d3147eecef06aff3a8ec
wonsunil <seonil6352@gmail.com>
2020-12-14 14:03:37 +0900

    Initial commit

---
+++ src/test/java/com/sunil/springeducation/SpringEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunil.springeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
