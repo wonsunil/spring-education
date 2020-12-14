00cd4ed920cf0733609a8a58559c667f977720e4
wonsunil <seonil6352@gmail.com>
2020-12-14 14:33:56 +0900

    Feat: add initializer

--- src/main/java/com/sunil/springeducation/service/UserService.java
+++ src/main/java/com/sunil/springeducation/service/UserService.java
@@ -1,5 +1,6 @@
 package com.sunil.springeducation.service;
 
+import com.sunil.springeducation.model.User;
 import com.sunil.springeducation.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,5 +12,30 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    };
+
+    public void initializeUsers() {
+        User user1 = User.builder()
+                .email("example@sample.com")
+                .name("Mr. Example")
+                .phone("01000000000")
+                .build();
+
+        User user2 = User.builder()
+                .email("example2@sample.com")
+                .name("Mrs. sample Data")
+                .phone("01000001234")
+                .build();
+
+        User user3 = User.builder()
+                .email("example3@sample.com")
+                .name("ms. Sample Data")
+                .phone("01012341234")
+                .build();
+
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+        this.userRepository.save(user3);
+        this.userRepository.flush();
     };
 }
