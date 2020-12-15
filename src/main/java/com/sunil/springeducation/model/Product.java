40cb36e7aaad762fec9886306c5e28583f3018ef
wonsunil <seonil6352@gmail.com>
2020-12-15 09:20:01 +0900

    Add: product jpa model

---
+++ src/main/java/com/sunil/springeducation/model/Product.java
@@ -0,0 +1,45 @@
+package com.sunil.springeducation.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(length = 80, nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private int listPrice;
+
+    @Column
+    private int price;
+
+
+    @Builder
+    public Product(String name, String description, int listPrice, int price) {
+        this.name = name;
+        this.description = description;
+        this.listPrice = listPrice;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[productId=%d, name='%s', description='%s', listPrice=%d, price=%d]",
+                this.productId, this.name, this.description, this.listPrice, this.price
+        );
+    }
+}
