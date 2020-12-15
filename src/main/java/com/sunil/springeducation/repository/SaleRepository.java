b4e74a04747cf06ee051b9efd93ceb35bfc3b617
wonsunil <seonil6352@gmail.com>
2020-12-15 09:52:41 +0900

    Add: sale repository

---
+++ src/main/java/com/sunil/springeducation/repository/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.sunil.springeducation.repository;
+
+import com.sunil.springeducation.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Integer> {
+
+}
