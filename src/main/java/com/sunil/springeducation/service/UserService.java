382fe770de070b13f8a0ba3135a7c50b26ee1469
wonsunil <seonil6352@gmail.com>
2020-12-15 12:08:49 +0900

    Refactor: upgrade sync

--- src/main/java/com/sunil/springeducation/service/UserService.java
+++ src/main/java/com/sunil/springeducation/service/UserService.java
@@ -2,6 +2,7 @@ package com.sunil.springeducation.service;
 
 import com.sunil.springeducation.model.User;
 import com.sunil.springeducation.repository.UserRepository;
+import com.sunil.springeducation.vo.UserRegisterVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -20,9 +21,7 @@ public class UserService {
     public User find(int userId) throws Exception{
         Optional<User> searchedUser = this.userRepository.findById(userId);
 
-        if(searchedUser == null) throw new Exception("해당 유저를 찾지 못하였습니다");
-
-        return searchedUser.get();
+        return searchedUser.orElseThrow(() -> new Exception("해당 유저를 찾지 못하였습니다"));
     };
 
     public List<User> findAll() {
@@ -51,6 +50,17 @@ public class UserService {
         this.userRepository.save(user1);
         this.userRepository.save(user2);
         this.userRepository.save(user3);
+        this.userRepository.flush();
+    };
+
+    public void createUser(UserRegisterVO user) {
+        User createUser = User.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .phone(user.getPhone())
+                .build();
+
+        this.userRepository.save(createUser);
         this.userRepository.flush();
     };
 }
