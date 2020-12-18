e439ed7e0ce60ee7d791a0f5eb26e3f390059150
wonsunil <seonil6352@gmail.com>
2020-12-18 10:44:25 +0900

    Feat: add column and constructor

--- src/main/java/com/sunil/springeducation/model/Coupon.java
+++ src/main/java/com/sunil/springeducation/model/Coupon.java
@@ -1,5 +1,6 @@
 package com.sunil.springeducation.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,6 +19,9 @@ public class Coupon {
     Date expireAt;
 
     @Column
+    int availableDays;
+
+    @Column
     String productId;
 
     @Column
@@ -28,4 +32,22 @@ public class Coupon {
 
     @Column
     int discountPercentage = 0;
+
+    @Builder
+    public Coupon(Date expireAt, int availableDays, String productId, String category, int discountPrice, int discountParentage) {
+        this.expireAt = expireAt;
+        this.availableDays = availableDays;
+        this.productId = productId;
+        this.category = category;
+        this.discountPrice = discountPrice;
+        this.discountPercentage = discountPercentage;
+    };
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Coupon[couponId=%d, expireAt='%s', availableDays=%d, productId=%d, category='%s', discountPrice=%d, discountPercentage=%d]",
+                this.couponId, this.expireAt, this.availableDays, this.productId, this.category. this.discountPrice, this.discountPercentage
+        );
+    };
 }
